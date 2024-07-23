a1e74505132cec2942310bd8c1f88872d415ed04
Nielcdn <101749698+Nielcdn@users.noreply.github.com>
2024-07-23 17:42:40 -0300

    Add files via upload

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,18 +6,25 @@ use App\Models\UsuariosModel;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        $data['variavel'] = "Conteudo do controller";
-        $data['nickname'] = "LordWenzel1978";
+        $data['username'] = "";
         $data['nome'] = "";
+        $data['email'] = "";
+        $data['senha'] = "";
+        $data['adm'] = FALSE;
         $this->session->set($data);
-        return view('my_view',$data);
+        return redirect()->to('/menu');
     }
 
-    public function apresenta_formulario(): string
+    public function formCadastro(): string
     {
         return view('cadastro');
+    }
+
+    public function formLogin(): string
+    {
+        return view('login');
     }
 
     public function telinha():string
@@ -26,29 +33,27 @@ class Home extends BaseController
         $my_model = new UsuariosModel();
         $result = $my_model->findAll();
 
-        //print_r($result);
         $data['result'] = $result;
-        $meuarray = $this->session->get();
-        $data['nickname'] = $meuarray['nickname']; 
-        print_r($data['result'][0]['nome']);
+        $infoAtual = $this->session->get();
+        $data['username'] = substr($infoAtual['email'],strpos($infoAtual['email'],'@')); 
+        echo "<br>";
 
-        $db      = \Config\Database::connect();
+        $db = \Config\Database::connect();
         $builder = $db->table('usuarios');
 
-
-        $id = $builder->select('id', )->get()->getResult('array');
-
-        return view('outra_view',$data);
+        $id = $builder->select('email, senha')->get()->getResult('array');
+        print_r(in_array(array('ewbriao1978@gmail.com'),$id));
+        print_r($id);
+        return view('menu',$data);
     }
 
-    public function receiveData()
+    public function recebeCadastro()
     {
         $data = array(
             'nome' => $this->request->getVar('nome'),
             'email' => $this->request->getVar('email'),
             'senha'=> $this->request->getVar('senha'),
         );
-
         $my_model = new UsuariosModel();
         $my_model->insert($data);
 
@@ -62,16 +67,42 @@ class Home extends BaseController
         //return view('view_formulario',$data);
     }
 
+    public function recebeLogin()
+    {
+        $data = array(
+            'email' => $this->request->getVar('email'),
+            'senha'=> $this->request->getVar('senha'),
+        );
+
+        $db = \Config\Database::connect();
+        $builder = $db->table('usuarios');
+
+        $id = $builder->select('email, senha')->where("email", $data['email'])->where("senha", $data['senha'])->get()->getResult('array');
+
+        if (sizeof($id)!=0){
+            $this->session->set($data);
+            if (in_array('ewbriao1978@gmail.com',$id[0]))
+
+            return redirect()->to('/menu');
+        }
+        else{
+            $this->session->setFlashdata('erro','Erro!! Email ou senha não estão corretos!!!!');
+            return redirect()->to('/login');
+        }
+        return redirect()->to('/menu');
+
+    }
+
     public function deletarItem()
     {
         $id = $this->request->getVar('id');
-        $my_model = new AutomoveisModel();
+        $my_model = new UsuariosModel();
         $my_model->delete($id);
         return redirect()->to('menu'); 
     }
     
     public function deletarItemPorURL($num){
-        $my_model = new AutomoveisModel();
+        $my_model = new UsuariosModel();
         $my_model->delete($num);
         return redirect()->to('menu'); 
 
@@ -79,7 +110,7 @@ class Home extends BaseController
 
     public function editarItem(){
 
-        $my_model = new AutomoveisModel();
+        $my_model = new UsuariosModel();
         $id_var = $this->request->getVar('id');
         $result = $my_model->find($id_var);
 
